0a1c29696207b200605887d6140db02c8a31aa61
rahmani1988 <rahhmani.mohammadreza@gmail.com>
2026-02-06 07:19:20 +0000

    working on keeping data in data store

---
+++ core/common/src/main/java/com/common/model/models/WeatherEntity.kt
@@ -0,0 +1,7 @@
+package com.common.model.models
+
+data class WeatherEntity(
+    val city: String,
+    val summary: String,
+    val timestamp: Long
+)
--- core/common/src/main/java/com/common/repository/WearSyncRepository.kt
+++ core/common/src/main/java/com/common/repository/WearSyncRepository.kt
@@ -1,9 +1,18 @@
 package com.common.repository
 
+import kotlinx.coroutines.flow.Flow
+
 interface WearSyncRepository {
     /**
-     * Syncs the current weather data to the wearable device.
+     * Syncs the current weather data to the wearable device. Mobile -> Wear
      * Returns [Result.Success] if the task was handed off to the system successfully.
      */
     suspend fun syncWeather(city: String, summary: String): Result<Unit>
+
+    /**
+     * Updates the local weather data. Wear Internal
+     */
+    suspend fun updateLocalWeather(city: String, summary: String)
+
+    fun getWeatherFlow(): Flow<WeatherModel?>
 }
