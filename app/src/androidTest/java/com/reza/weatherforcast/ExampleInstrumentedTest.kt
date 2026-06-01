08ba9f4579e2dbbfd51a908c59e9f8646c19d249
rahmani1988 <rahhmani.mohammadreza@gmail.com>
2026-06-01 16:45:46 +0100

    bug fixing the ui test

--- app/src/androidTest/java/com/reza/weatherforcast/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/reza/weatherforcast/ExampleInstrumentedTest.kt
@@ -1,12 +1,8 @@
-package com.weatherforcast
+package com.reza.weatherforcast
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +15,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.weatherforcast", appContext.packageName)
+        Assert.assertEquals("com.reza.weatherforcast", appContext.packageName)
     }
 }
